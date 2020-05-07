2564924174fce122683d329992d923a4e6248d4e
james Johnson <2013matthew2013@gmail.com>
2020-05-07 19:10:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma (text)
   IF text == "I LOVE YOU TO GRANDMA!"
      return "I LOVE YOU TOO PUMPKIN!"
   Elsif 
-       text== = nil && text.upcase == text
+       text = nil && text.upcase == text
      return "NO, NOT SINCE 1938!"
   ELSE 
      return "HUH?! SPEAK UP, SONNY!"
