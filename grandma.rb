3574cdf7cc19e14ee82f1b357f3bc21d1de9284c
james Johnson <2013matthew2013@gmail.com>
2020-05-07 18:59:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,3 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
+def speak_to_grandma
+  IF phase == "I LOVE YOU TO GRANDMA!"
+     return "I LOVE YOU TOO PUMPKIN!"
+     elsif PHASE == phase.DOWNCASE
+    "NO, NOT SINCE 1938!"
+  ELSE "HUH?! SPEAK UP, SONNY!"
+  end
+end
